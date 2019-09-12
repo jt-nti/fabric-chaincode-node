2d668cea20d4f53bdbe50d3c3a802e07297c3f0f
Matthew B White <whitemat@uk.ibm.com>
2019-09-12 14:32:20 +0100

    [FAB-16635] Refactor to RUSH
    Signed-off-by: Matthew B White <whitemat@uk.ibm.com>
    Change-Id: Ie873615a4285909c082abdc3c80a77439dda6850

--- test/fv/annotations.js
+++ test/fv/annotations.js
@@ -44,7 +44,7 @@ describe('Typescript chaincode', () => {
             this.timeout(SHORT_STEP);
             const payload = JSON.parse(await utils.query(suite, 'org.hyperledger.fabric:GetMetadata'));
 
-            const schema = fs.readFileSync(path.join(__dirname, '../../fabric-contract-api/schema/contract-schema.json'));
+            const schema = fs.readFileSync(path.join(__dirname, '../../apis/fabric-contract-api/schema/contract-schema.json'));
 
             const ajv = new Ajv({schemaId: 'id'});
             ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-04.json'));
